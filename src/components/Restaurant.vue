5dfabaa041a980d6a1d1a7335afaf800656f7ac5
deflessc <camille.deflesselle@edu.esiee.fr>
2021-09-22 13:43:18 -0400

    page restaurant (manque itinéraire)

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
+  <div id="restaurantpage">
     <h1>Restaurant</h1>
-    <div>GLO-3102 Restaurant page</div>
-    <div id="restaurantpage">
-    <div><option>Restaurant</option><div>Mission Chinese Food</div></div><div><option>Address</option><div>171 E Broadway, New York, NY 10002</div></div><div><li><div>Monday : 5:30 pm - 11:00 pm</div></li><li><div>Tuesday : 5:30 pm - 12:00 am</div></li><li><div>Wednesday : 5:30 pm - 12:00 am</div></li><li><div>Thursday : 5:30 pm - 12:00 am</div></li><li><div>Friday : 5:30 pm - 12:00 am</div></li><li><div>Saturday : 12:00 pm - 4:00 pm, 5:30 pm - 12:00 am</div></li><li><div>Sunday : 12:00 pm - 4:00 pm, 5:30 pm - 11:00 pm</div></li></div><div><option>Cuisine</option><div>Asian</div></div><div><option>Tel</option><div>546 678 234</div></div><div><option>Itinéraire</option><div>itinéraire</div></div><div><option>Price</option><div>45 $</div></div><div><option>Rate</option><div>4.5/5</div></div></div>
-  </div>
+    <div><option>Restaurant</option><div>Mission Chinese Food</div></div><div><option>Address</option><div>171 E Broadway, New York, NY 10002</div></div><div><li><div>Monday : 5:30 pm - 11:00 pm</div></li><li><div>Tuesday : 5:30 pm - 12:00 am</div></li><li><div>Wednesday : 5:30 pm - 12:00 am</div></li><li><div>Thursday : 5:30 pm - 12:00 am</div></li><li><div>Friday : 5:30 pm - 12:00 am</div></li><li><div>Saturday : 12:00 pm - 4:00 pm, 5:30 pm - 12:00 am</div></li><li><div>Sunday : 12:00 pm - 4:00 pm, 5:30 pm - 11:00 pm</div></li></div><div><option>Cuisine</option><div>Asian</div></div><div><option>Tel</option><div>546 678 234</div></div><div><option>Itinéraire</option><div>itinéraire</div></div><div><option>Price</option><div>45 $</div></div><div><option>Rate</option><div>4.5/5</div></div>
+    </div>
 </template>
 
 <style>
@@ -14,9 +12,8 @@ pageres{
     align-content:center;
     justify-items:center;
     flex-direction:column;
-    margin-left: 10%;
+   
     margin-right:10%;
-    background-color: rgb(223, 223, 223);
 }
 
 
@@ -50,7 +47,6 @@ pageres{
     overflow: auto;
    
     position: absolute;
-    background-color: #f1f1f1;
     
     box-shadow: 0% 5% 10% 0% rgba(0,0,0,0.2);
     z-index: 1;
@@ -63,7 +59,6 @@ pageres{
     padding: 2%; /* Add some padding */
     text-decoration: none; /* Remove default text underline */
     
-    padding-left:5%;
   }
   #restaurantpage div div {
     flex:5;
