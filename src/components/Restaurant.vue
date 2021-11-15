df7277194c001c8d3c50cf36f3974c3c4548104f
AntoineLaviolette <antoine.laviolette.1@ulaval.ca>
2021-11-14 20:32:17 -0500

    pagerestaurant permanante apres refresh

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -1,104 +1,102 @@
 <template>
-  <body>
-    <div id="restaurantpage">
-      <footer>
-        <b-dropdown
-          text="Ajouter Favoris"
-          variant="primary"
-          class="m-2"
-          size="lg"
+  <div id="restaurantpage">
+    <footer>
+      <b-dropdown
+        text="Ajouter Favoris"
+        variant="primary"
+        class="m-2"
+        size="lg"
+      >
+        <b-dropdown-item
+          v-for="list in listResto"
+          :key="list.id"
+          @click="addRestaurantToList(list.id, restaurant.id)"
         >
-          <b-dropdown-item
-            v-for="list in listResto"
-            :key="list.id"
-            @click="addRestaurantToList(list.id, restaurant.id)"
-          >
-            {{ list.name }}
-          </b-dropdown-item>
-        </b-dropdown>
-        <button class="button" @click="openModal(restaurantId)">
-          Entrer visit
-        </button>
-      </footer>
-      <div class="ligne">
-        <option>Restaurant</option>
-        <div>{{ restaurant.name }}</div>
-      </div>
-      <div class="ligne">
-        <option>Cuisine</option>
-        <div>
-          <ul>
-            <li v-for="genre in restaurant.genres" :key="genre">
-              {{ genre }}
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="ligne">
-        <option>Price</option>
-        <div>
-          <Price :priceTag="restaurant.price_range"></Price>
-        </div>
-      </div>
-      <div class="ligne">
-        <option>Rate</option>
-        <div>
-          <Stars :rating="restaurant.rating"></Stars>
-        </div>
-      </div>
-      <div class="ligne">
-        <option>Opening hours</option>
-        <div>
-          <ul>
-            <div
-              class="small"
-              v-for="(hour, day) in restaurant.opening_hours"
-              :key="day"
-            >
-              {{ day }} : {{ hour }}
-            </div>
-          </ul>
-        </div>
-      </div>
-      <div class="ligne">
-        <option>Adresse</option>
-        <div>{{ restaurant.address }}</div>
-      </div>
-      <div class="ligne">
-        <option>Telephone</option>
-        <div>{{ restaurant.tel }}</div>
-      </div>
-      <Modal v-if="isModalVisible">
-        <template v-slot:m-header> Ajout d'une visite </template>
-        <template v-slot:m-body>
-          <div>Date de la visite</div>
-          <Datepicker v-model="date"></Datepicker>
-          <div>Commentaire:</div>
-          <textarea
-            name="commentArea"
-            id="comment"
-            placeholder="Entrez votre commentaire ici"
-            v-model="comment"
-          ></textarea>
-          <div>
-            <label for="rating">Votre cote :</label>
-            <select name="rating" id="rating" v-model="rating">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-          </div>
-        </template>
-        <template v-slot:m-footer>
-          <div class="modal-button-area">
-            <span><button @click="submitVisit">Soumettre</button></span>
-            <span><button @click="closeModal">Annuler</button></span>
-          </div>
-        </template>
-      </Modal>
+          {{ list.name }}
+        </b-dropdown-item>
+      </b-dropdown>
+      <button class="button" @click="openModal(restaurantId)">
+        Entrer visit
+      </button>
+    </footer>
+    <div class="ligne">
+      <option>Restaurant</option>
+      <div>{{ restaurant.name }}</div>
     </div>
+    <div class="ligne">
+      <option>Cuisine</option>
+      <div>
+        <ul>
+          <li v-for="genre in restaurant.genres" :key="genre">
+            {{ genre }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="ligne">
+      <option>Price</option>
+      <div>
+        <Price :priceTag="restaurant.price_range"></Price>
+      </div>
+    </div>
+    <div class="ligne">
+      <option>Rate</option>
+      <div>
+        <Stars :rating="restaurant.rating"></Stars>
+      </div>
+    </div>
+    <div class="ligne">
+      <option>Opening hours</option>
+      <div>
+        <ul>
+          <div
+            class="small"
+            v-for="(hour, day) in restaurant.opening_hours"
+            :key="day"
+          >
+            {{ day }} : {{ hour }}
+          </div>
+        </ul>
+      </div>
+    </div>
+    <div class="ligne">
+      <option>Adresse</option>
+      <div>{{ restaurant.address }}</div>
+    </div>
+    <div class="ligne">
+      <option>Telephone</option>
+      <div>{{ restaurant.tel }}</div>
+    </div>
+    <Modal v-if="isModalVisible">
+      <template v-slot:m-header> Ajout d'une visite </template>
+      <template v-slot:m-body>
+        <div>Date de la visite</div>
+        <Datepicker v-model="date"></Datepicker>
+        <div>Commentaire:</div>
+        <textarea
+          name="commentArea"
+          id="comment"
+          placeholder="Entrez votre commentaire ici"
+          v-model="comment"
+        ></textarea>
+        <div>
+          <label for="rating">Votre cote :</label>
+          <select name="rating" id="rating" v-model="rating">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+      </template>
+      <template v-slot:m-footer>
+        <div class="modal-button-area">
+          <span><button @click="submitVisit">Soumettre</button></span>
+          <span><button @click="closeModal">Annuler</button></span>
+        </div>
+      </template>
+    </Modal>
     <div class="listimgres">
       <img
         class="imgres"
@@ -111,50 +109,21 @@
     <div class="localisation">
       <Map :coord="restaurant.location.coordinates"></Map>
     </div>
-  </body>
+  </div>
 </template>
 <script>
-import Vue from "vue";
-import Vuex from "vuex";
-import axios from "axios";
+import store from "./store/requests.js";
 import Map from "./map.vue";
 import Stars from "./stars.vue";
 import Price from "./price.vue";
 import Modal from "./Modal.vue";
 import Datepicker from "vuejs-datepicker";
 import {
-  restaurantsFiltered,
-  createVisit,
-  visitesOfOneRestaurantByUser,
-  visitesRestaurantOfUser,
-} from "./api/restaurants.js";
-
-import {
+  getListFavorites,
   addRestaurantToList,
   viewListFavorites,
-  getListFavorites,
-} from "./api/favorites";
-Vue.use(Vuex);
-
-const storeRes = new Vuex.Store({
-  state: {
-    info: undefined,
-  },
-  mutations: {
-    setInfo(state, data) {
-      state.info = { ...data };
-    },
-  },
-  actions: {
-    async getInfo({ commit }, id) {
-      const response = await axios.get(
-        `https://ufoodapi.herokuapp.com/unsecure/restaurants/${id}`,
-        {}
-      );
-      commit("setInfo", response.data);
-    },
-  },
-});
+} from "./api/favorites.js";
+import { visitesRestaurantOfUser } from "./api/restaurants.js";
 
 export default {
   name: "Restaurant",
@@ -166,7 +135,7 @@ export default {
     Modal,
     Datepicker,
   },
-  data: function () {
+  data: () => {
     return {
       isModalVisible: false,
       filterGenres: [],
@@ -178,7 +147,7 @@ export default {
   },
   computed: {
     restaurant() {
-      return storeRes.state.info;
+      return this.$store.state.info_restaurant;
     },
     listResto() {
       return this.$store.state.ListFavorites.items;
@@ -228,14 +197,21 @@ export default {
     },
   },
   mounted() {
-    storeRes.dispatch("getInfo", this.restaurantId);
+    if (this.restaurantId) {
+      window.localStorage.setItem("restoId", this.restaurantId);
+      this.$store.dispatch("getInfoRestaurant", this.restaurantId);
+    } else {
+      this.$store.dispatch(
+        "getInfoRestaurant",
+        window.localStorage.getItem("restoId")
+      );
+    }
     this.$store.dispatch("getList");
   },
 };
 </script>
 <style>
 /*page restaurant*/
-html,
 body {
   height: 100%;
   font-family: "Montserrat";
